1d3c0b92b51348a376154641344cbc3035e2d8f3
Darius <speed.fanat1c@gmail.com>
2023-07-13 20:02:32 +0300

    Using tarlepp docker nginx config

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -7,13 +7,13 @@ use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Workflow\WorkflowInterface;
+//use Symfony\Component\Workflow\WorkflowInterface;
 
 class ArticlesController extends AbstractController
 {
     public function __construct(
 //        #[Target('blog_publishing')]
-        private WorkflowInterface $workflow,
+//        private WorkflowInterface $workflow,
         private ArticleRepository $articleRepository
     ) {
     }
